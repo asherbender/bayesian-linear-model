c4ccd4b63564c52391549f224075c2760db2c682
Asher Bender <a.bender.dev@gmail.com>
2015-02-22 16:59:56 +1100

    BayesianLinearRegression: Model template added
        - Empty class methods added. sklearn learn has a Bayesian linear
          model (sklearn.linear_model.BayesianRidge) with the following
          methods:
    
            - decision_function
            - fit
            - get_params
            - predict
            - score
            - set_params
    
          This aim of this project is to more closely follow nomenclature in
          the machine learning community. The methods chosen to represent
          this object will not necessarily correspond with the methods used
          in sklearn.

---
+++ linear_model.py
@@ -0,0 +1,26 @@
+"""Bayesian Linear Model.
+
+References:
+
+   [1] Murphy, K. P., Machine learning: A probabilistic perspective,
+       The MIT Press, 2012
+
+.. sectionauthor:: Asher Bender <a.bender.dev@gmail.com>
+.. codeauthor:: Asher Bender <a.bender.dev@gmail.com>
+
+"""
+
+
+class BayesianLinearRegression(object):
+
+    def __init__(self):
+        pass
+
+    def update(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def evidence(self):
+        pass
